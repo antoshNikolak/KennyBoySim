fb75252396bef7bf75945a1c00faf45ef3b5052e
antoshNikolak <antoninikolak@gmail.com>
2021-06-12 20:33:04 +0100

    ok

--- src/main/java/Game/AppLauncher.java
+++ src/main/java/Game/AppLauncher.java
@@ -3,6 +3,7 @@ package Game;
 public class AppLauncher {
     public static void main(String[] args) {
         System.out.println("wagwan");
+        System.out.println("Hello");
         App.main(args);
 
     }
